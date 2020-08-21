dd5583dd1fb1426663928fdb72d057b7b5509572
Nico Michel <nicomichel@utexas.edu>
2020-08-21 10:42:54 -0500

    built and styled homepage

--- employeedirectory/src/App.js
+++ employeedirectory/src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from './components/Header';
+import Jumbotron from './components/Jumbotron';
+import Handshake from './assets/bigHandshake.jpg'; 
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+<Header />
+<Jumbotron />
+<img src={Handshake} className = "handshake" alt="handshake" roundedCircle />
     </div>
-  );
-}
+  )}
 
 export default App;
---
+++ employeedirectory/src/components/Jumbotron.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Jumbotron () {
+    return (
+        <div className="jumbotron">
+        <h1>Click The Button Below To See Employee Table!</h1>
+        <p>
+          Enim adipisicing enim reprehenderit ex ullamco consectetur Lorem laboris magna exercitation.
+          Aliquip reprehenderit magna elit cillum adipisicing dolore aliquip velit. Ipsum ullamco
+          nostrud tempor eu deserunt ipsum incididunt fugiat esse ipsum. Exercitation nostrud
+          exercitation sit ex nostrud aliqua officia magna nostrud deserunt et esse eu deserunt. Non
+          dolor consequat qui ea culpa tempor magna nulla consectetur est occaecat.
+        </p>
+        <p>
+        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+        </p>
+      </div>
+)};
+
+export default Jumbotron;
