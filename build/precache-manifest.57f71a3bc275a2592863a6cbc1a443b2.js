3831547e98ca47399390f6b80a361c9bd5106b57
Nico Michel <nicomichel@utexas.edu>
2020-08-22 19:45:05 -0500

    trying to deploy

---
+++ build/precache-manifest.57f71a3bc275a2592863a6cbc1a443b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a3eea505dd659e0b9e0932f180e6f58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12b790af7b3001bba26d",
+    "url": "/static/css/main.9edb8c18.chunk.css"
+  },
+  {
+    "revision": "bf1bb2f57f8b5c9f7a25",
+    "url": "/static/js/2.7a133c2c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7a133c2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12b790af7b3001bba26d",
+    "url": "/static/js/main.cb155edd.chunk.js"
+  },
+  {
+    "revision": "35839d639db8e60032a1",
+    "url": "/static/js/runtime-main.f78ad96b.js"
+  },
+  {
+    "revision": "6a1f0ae5bbce29cd5146867584580aa7",
+    "url": "/static/media/bigHandshake.6a1f0ae5.jpg"
+  }
+]);
